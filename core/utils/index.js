081385e7bd6005174b55751abb759bc5dc337eb5
ruj <214782@bk.ru>
2021-04-14 20:32:42 -0300

    What current environment

---
+++ core/utils/index.js
@@ -0,0 +1,18 @@
+export default class Utils {
+  static whatEnv (value) {
+    const env = process.env.NODE_ENV
+
+    if (typeof value === 'string' && value !== env) {
+      process.emitWarning(
+        'Different environment than requested' +
+          (value && ` (${value.toUpperCase()})`),
+        {
+          code: 'WHAT_ENV',
+          detail: `The current environment is ${env.toUpperCase()}`
+        }
+      )
+    }
+
+    return env
+  }
+}
