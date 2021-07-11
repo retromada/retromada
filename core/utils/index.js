66e2fe7a30bc65ccb9f343c286cb913d98d8e539
ruj <214782@bk.ru>
2021-07-10 22:08:42 -0300

    Option handler

--- core/utils/index.js
+++ core/utils/index.js
@@ -1,6 +1,34 @@
 import Toolbox from '@retromada/toolbox'
 
 export default class Utils {
+  static optionHandler (structure, options = {}) {
+    return {
+      default (name, defaultValue) {
+        const value = options[name]
+
+        return typeof value === 'undefined'
+          ? Array.isArray(defaultValue)
+            ? defaultValue.random()
+            : defaultValue
+          : value
+      },
+
+      optional (name) {
+        return options[name] || null
+      },
+
+      required (name) {
+        const value = options[name]
+
+        if (typeof value === 'undefined') {
+          throw new Error(`The "${name}" option in the ${structure} structure is required`)
+        }
+
+        return value
+      }
+    }
+  }
+
   static whatEnv (value) {
     const env = process.env.NODE_ENV
 
