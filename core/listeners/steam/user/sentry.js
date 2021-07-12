f97d5481921c0d93aff41810b1a643b3650edea0
ruj <214782@bk.ru>
2021-07-12 00:29:33 -0300

    Debugging the sentry when it is updated in the database

--- core/listeners/steam/user/sentry.js
+++ core/listeners/steam/user/sentry.js
@@ -5,5 +5,19 @@ export default class Sentry extends Listener {
     super(client)
   }
 
-  onSentry (sentry) {}
+  onSentry (sentry) {
+    const labels = ['Steam', 'User', 'sentry']
+
+    this.client.database.emulators
+      .update({ seq: this.client.seq })
+      .then(() =>
+        this.logger.debug(
+          {
+            labels
+          },
+          `Sentry written to ${this.client.steamID}`
+        )
+      )
+      .catch((error) => this.logger.error({ labels }, error))
+  }
 }
